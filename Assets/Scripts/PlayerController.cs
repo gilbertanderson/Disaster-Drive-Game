6cb246e852ff5f8540f8833b0428b3184db5f3a2
Gilbert Anderson <gilbertjanderson@gmail.com>
2026-06-30 02:06:24 -0500

    Implement player movement setup in PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5f;
+    public InputAction movementAction;
+    private Rigidbody playerRb;
+    private Vector2 movementInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        movementAction.Enable();
     }
 
     // Update is called once per frame
